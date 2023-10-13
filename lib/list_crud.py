fb421258210a95bf708a7155bf9bf87c04dbf8d3
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-10-13 15:23:37 +0300

    Assignment complete

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,55 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return ["Boba", "Latte", "Macha", "Mocha"]
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0,element)
+    return l
 
 def remove_element_from_end_of_list(l):
-    return None
+    l.pop()
+    return l
 
 def remove_element_from_start_of_list(l):
-    return None
+     l.pop(0)
+     return l
+
 
 def retrieve_first_element_from_list(l):
-    return None
+   first_element = l[0]
+   return first_element
 
 def retrieve_element_from_index(l, index):
-    return None
+    element = l[index]
+    return element
 
 def retrieve_last_element_from_list(l):
-    return None
+    last_element = l[-1]
+    return last_element
+
+append_element = add_element_to_end_of_list([1,2,3], 4)
+print(append_element)
+
+insert_element = add_element_to_start_of_list(["April","Lily","Mae"], "Charlie")
+print(insert_element)
+
+remove_last_element = remove_element_from_end_of_list([99.9,67,77.5])
+print(remove_last_element)
+
+remove_first_element = remove_element_from_start_of_list([99.9,67,77.5])
+print(remove_first_element)
+
+retrieve_first_element = retrieve_first_element_from_list(["happy","cheerful","joyous"])
+print(retrieve_first_element)
+
+retrieve_from_index = retrieve_element_from_index(["April","Lily","Mae"], 0)
+print(retrieve_from_index)
+
+retrieve_last_element = retrieve_last_element_from_list(["April","Lily","Mae"])
+print(retrieve_last_element)
